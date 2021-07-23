9d229da33c0f2480f68e2bae9e2e70f880c2d996
Ken Javier <kencarlo21@gmail.com>
2021-07-23 12:31:17 +0800

    Hero section  modified(1)

--- src/components/sections/Hero/Hero.js
+++ src/components/sections/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as styles from './styles/css/hero.module.css';
 import myPhoto from "../../../assets/images/jpg-png/Me Edited w Shadow.png";
 import scrollTo from 'gatsby-plugin-smoothscroll';
@@ -9,12 +9,38 @@ import linkedin from "../../../assets/images/svg/LinkedIn.svg";
 import github from "../../../assets/images/svg/GitHub.svg";
 
 const Hero = () => {
+
+    let [innerWidth, setInnerWidth] = useState(768);
+
+    useEffect(() => {
+        window.addEventListener('load', getInnerWidth);
+        window.addEventListener('mouseover', getInnerWidth);
+        window.addEventListener('resize', getInnerWidth);
+
+        return () => {
+
+            window.removeEventListener('load', getInnerWidth);
+            window.removeEventListener('mouseover', getInnerWidth);
+            window.removeEventListener('resize', getInnerWidth);
+        }
+    }, []);
+    
+    // ==== Determines the width of the website ====
+    const getInnerWidth = () => {
+        setInnerWidth(window.innerWidth);
+    };
+
     return (
         <div className={styles.heroSection}>
 
-            <div className={styles.rightPart}>
-                <h3>Hello! My name is Ken!</h3>
-                <h1>I am a <span>Web Developer</span></h1>
+            <div className={styles.leftPart}>
+                { innerWidth > 535 ? <>
+                                        <h3>Hello, My name is Ken!</h3>
+                                        <h1>I am a <span>Web Developer</span></h1>
+                                    </> : <> <h3>Hello, My name is Ken! I am a</h3>
+                                        <h1><span>Web Developer</span></h1> </>              
+                 }
+                
 
                 <div className={styles.socialButtons}>
                     <a href="https://www.facebook.com/kcjavier21/" target="_blank" rel="noreferrer"><img src={facebook} alt="Ken Javier on Facebook" /></a>
@@ -23,6 +49,7 @@ const Hero = () => {
                     <a href="https://www.instagram.com/kcjavier21/" target="_blank" rel="noreferrer"><img src={instagram} alt="Ken Javier on Facebook" /></a>
                 </div>
 
+
                 <div className={styles.buttonContainer}>
                     <button onClick={() => scrollTo(`#projects`)} className={styles.seeMyWork} >See My Work</button>
                     <button onClick={() => scrollTo(`#projects`)} className={styles.getMyResume}>Get My Resume</button>
@@ -30,6 +57,7 @@ const Hero = () => {
                 </div>
             </div>
 
+            <div className={styles.shade}/>
 
             <img
                 className={styles.heroImage}
@@ -39,14 +67,6 @@ const Hero = () => {
                 // layout="constrained"
             />
             
-            {/* <StaticImage
-                className={styles.heroImage}
-                src="../../../assets/images/jpg-png/Me Edited w Shadow.png"
-                alt="Ken Javier"
-                placeholder="blurred"
-                layout="constrained"
-                width={800}
-            /> */}
         </div>
     )
 }
