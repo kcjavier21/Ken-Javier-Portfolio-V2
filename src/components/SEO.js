2733bdaccec545bca29b5679a680277ac6fa9ab7
Ken Javier <kencarlo21@gmail.com>
2021-11-17 15:48:05 +0800

    Added Thumbnail Image

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,10 +1,12 @@
 import React from "react"
 import {Helmet} from "react-helmet"
+import thumbnail from "../assets/images/jpg-png/thumbnail.png"
 
 const SEO = () => {
     return (
         <Helmet>
             <title>Ken Javier</title>
+            <meta property="og:image" content={thumbnail}/>
         </Helmet>
     )
 }
