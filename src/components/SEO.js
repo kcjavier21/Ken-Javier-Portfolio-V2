d6aae4af2fbaefb03bba54af58030bfa79efd526
Ken Javier <kencarlo21@gmail.com>
2021-11-18 08:52:59 +0800

    Fixed preview

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,15 +1,27 @@
 import React from "react"
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet"
 import thumbnail from "../assets/images/jpg-png/thumbnail.png"
 
 const SEO = () => {
-    return (
-        <Helmet>
-            <meta name="title" content="Ken Javier" />
-            <meta name="image" content={thumbnail}/>
-            <title>Ken Javier</title>
-        </Helmet>
-    )
+  return (
+    <Helmet
+      title="Ken Javier"
+      meta={[
+        {
+          property: `og:title`,
+          content: "Ken Javier",
+        },
+        {
+          property: `og:type`,
+          content: `portfolio`,
+        },
+        {
+          property: `og:image`,
+          content: thumbnail,
+        },
+      ]}
+    />
+  )
 }
 
 export default SEO
